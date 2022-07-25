c9992d52a1d63e10973c2e0a3fd928ea153bf60c
Nehal Gund <gundnehal@gmail.com>
2022-07-25 21:00:16 +0530

    Update BMIcalculation.py
    BMI Calculation Program with various units.
    Second Assignment

--- BMIcalculation.py
+++ BMIcalculation.py
@@ -1,79 +1,75 @@
 # Body Mass Index Calculator
 while True:
     try:
-        print("""\t******Body Mass Index*******
-\tSelect Weight Unit:""")
-        weight = ['Kilograms','Ponds']
-        a = 0
-        while a < len(weight):
-            print("\t {0}. {1}".format(a+1,weight[a]))
-            a += 1
-        response = int(input("\tEnter your choice: "))
-        print()
-        if response in range(len(weight)):
-            if response == 1:
-                kg = float(input("\tEnter your weight in kg: "))
-                wt = kg
-            elif response == 2:
-                lbs = float(input("\tEnter your weight in lb: "))
-                wt = lbs / 2.205  
-
-            print("\n\tSelect Height Unit:")
-            height = ['Centimeters','Meters','Foot','Inches','Foot & Inches']
-            b = 0
-            while b < len(height):
-                print("\t {0}. {1}".format(b+1,height[b]))
-                b += 1
-            response = int(input("\tEnter your choice: "))
+        print("\t******Body Mass Index*******")
+        while True:
+            print("\tSelect Weight Unit:")
+            weight = ['Kilograms','Pounds','Exit']
+            a = 0
+            while a < len(weight):
+                print("\t {0}. {1}".format(a+1,weight[a]))
+                a += 1
+            response1 = int(input("\tEnter choice number: "))
             print()
-            if response == 1:
-                cm = float(input("\tEnter your height in 'cm: "))
-                ht = cm
-            elif response == 2:
-                m = float(input("\tEnter your height in 'm': "))
-                ht = m * 100
-            elif response == 3:
-                ft = float(input("\tEnter your height in 'foot': "))
-                ht = ft * 30.48
-            elif response == 4:
-                inc = float(input("\tEnter your height in 'inches': "))
-                ht = inc * 2.54
-            elif response == 5:
-                ft = float(input("\tEnter your height in 'foot': ")) * 30.48
-                inc = float(input("\tEnter your height in 'inches': ")) * 2.54
-                ht = float(ft) + float(inc)
-            else:
-                print("""\t****************************
-\tIncorrect Choice""")
-                wt = 0
-        else:
-            print("""\t****************************
-\tIncorrect Choice""")
-            wt = 0
-        if wt > 0:
-            bmi = wt * (100**2) / (ht**2)
-            print("""\t****************************
-\tYour BMI is : {0:.2f}""".format(bmi))
-            
-            if bmi < 18.5 and bmi > 0:
-                print("\tOops!, You are Underweight")
-            elif bmi >= 18.5 and bmi <= 25:
-                print("\tCongrats! Your BMI is Normal")
-            elif bmi>25:
-                print("\tYou are Overweight, Take Care!")
-            else:
-                print("\tCan't Estimate")
-            print("""\t****************************
-\t\t Thank You
-\t****************************""")
-        else:
-            print("\tCan't Estimated")
-            print("""\t****************************
-\t\t Try Again!
-\t****************************""")    
+            if response1 == 1:
+                kg = float(input("\tEnter weight in kg: "))
+                wt = kg
+            elif response1 == 2:
+                lbs = float(input("\tEnter weight in lb: "))
+                wt = lbs / 2.205
+            elif response1 == 3:
+                break
+            elif response1 <= 0 or response1 > len(weight):
+                print("\t*******************************\n\tIncorrect Choice! Select Again!")
+                continue
+            while True:
+                print("\tSelect Height Unit:")
+                height = ['Centimeters','Meters','Foot','Inches','Foot & Inches','Exit']
+                b = 0
+                while b < len(height):
+                    print("\t {0}. {1}".format(b+1,height[b]))
+                    b += 1
+                response2 = int(input("\tEnter choice number: "))
+                print()
+                if response2 == 1:
+                    cm = float(input("\tEnter  height in 'cm: "))
+                    ht = cm
+                elif response2 == 2:
+                    m = float(input("\tEnter height in 'm': "))
+                    ht = m * 100
+                elif response2 == 3:
+                    ft = float(input("\tEnter height in 'foot': "))
+                    ht = ft * 30.48
+                elif response2 == 4:
+                    inc = float(input("\tEnter height in 'inches': "))
+                    ht = inc * 2.54
+                elif response2 == 5:
+                    ft = float(input("\tEnter height in 'foot': ")) * 30.48
+                    while True:    
+                        inc = float(input("\tEnter height in 'inches': "))
+                        if inc <= 12:
+                            ht = ft + (inc * 2.54)
+                        else:
+                            print("\tInches must be between '0 to 12'.\n\tRe-enter!")
+                            continue
+                        break
+                elif response2 == 6:
+                    break
+                elif response2 <= 0 or response2 > len(height):
+                    print("\t*******************************\n\tIncorrect Choice! Select Again!")
+                    continue
+                bmi = float(wt) / float(ht) / float(ht) * 100 * 100
+                print("""\t****************************""")
+                if bmi < 18.5 and bmi > 0:
+                    print("\tOops!, You are Underweight")
+                elif bmi >= 18.5 and bmi <= 25:
+                    print("\tCongrats! BMI is Normal")
+                elif bmi>25:
+                    print("\tYou are Overweight, Take Care!")
+                print("\tYour BMI is : {0:.2f}\n\t****************************\n\t\t Thank You\n\t****************************\n".format(bmi))   
+                break
+            break
     except Exception:
-        print("""\t****************************
-\t\t Try Again!
-\t****************************""")
+        print("\t********************************\n\tSomething went Wrong! Try Again!\n\t********************************\n")
     finally:
-        input("\nPress 'Enter' to calculate BMI ")
+        input("Press 'Enter' to calculate BMI ")
